394028a71ccffda1d679841c64076506bc8d7b22
PedroDeVvV <pedrohgs2004@gmail.com>
2024-12-10 10:03:19 -0300

    REFACTIR: rename env variables

--- src/repository/connection.js
+++ src/repository/connection.js
@@ -4,9 +4,9 @@ async function connect() {
   const connection = await mysql2.createConnection({
     host: process.env.HOST_DB,
     password: process.env.PASSWORD_DB,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     database: process.env.DATABASE,
-    user: process.env.USER
+    user: process.env.DB_USER
   });
   return connection;
 }
